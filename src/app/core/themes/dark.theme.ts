3eb49b01856d69d6912b69ccf0b92207662e7f63
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-06-11 11:51:19 +0200

    Theme constants added

--- src/app/core/themes/dark.theme.ts
+++ src/app/core/themes/dark.theme.ts
@@ -1,4 +1,12 @@
 export const DarkTheme: {[key: string]: string} = {
-  '--primary': 'black',
-  '--bg-primary': 'green',
+  '--body-bg': 'rgb(43, 44, 55)',
+  '--text-primary': 'rgb(255, 255, 255)',
+  '--text-secondary': 'rgb(99, 95, 195)',
+  '--bg-primary': 'rgb(99, 95, 195)',
+  '--bg-primary-hover': 'rgb(168, 164, 255)',
+  '--bg-secondary': 'rgba(99, 95, 195, .1)',
+  '--bg-secondary-hover': 'rgba(99, 95, 195, .25)',
+  '--bg-danger': 'rgb(234, 85, 85)',
+  '--bg-danger-hover': 'rgb(255, 152, 152)',
+  '--bg-primary-box': 'rgb(32, 33, 34)',
 };
