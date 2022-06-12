a0822dd82af7b89bbf66f0766fb488be7781373d
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-06-12 09:59:00 +0200

    Board module and components generated

---
+++ src/app/pages/boards/components/add-edit-board-dialog/add-edit-board-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-board-dialog',
+  templateUrl: './add-edit-board-dialog.component.html',
+  styleUrls: ['./add-edit-board-dialog.component.scss']
+})
+export class AddEditBoardDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/boards/components/add-edit-task-dialog/add-edit-task-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-task-dialog',
+  templateUrl: './add-edit-task-dialog.component.html',
+  styleUrls: ['./add-edit-task-dialog.component.scss']
+})
+export class AddEditTaskDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
