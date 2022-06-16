ced95a4d02d8c6d7ddf724a36f02893f6180ba49
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-06-16 19:35:58 +0200

    Board name displayed in navbar upon selection

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,6 +6,8 @@ import {LayoutStoreFacade} from '../../core/store/layout/layout-store.facade';
 import {
   AddEditTaskDialogComponent
 } from '../../pages/boards/components/add-edit-task-dialog/add-edit-task-dialog.component';
+import {Board} from '../../pages/boards/core/interfaces';
+import {BoardsStoreFacade} from '../../pages/boards/core/store/boards-store.facade';
 
 @Component({
   selector: 'app-navbar',
@@ -14,10 +16,13 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent implements OnInit {
+  currentBoard$: Observable<Board | undefined> = this.boardsStoreFacade.currentBoard$;
   currentTheme$: Observable<Theme> = this.layoutStoreFacade.getTheme$;
   isSidenavClosed$: Observable<boolean> = this.layoutStoreFacade.getIsSidenavClosed$;
 
-  constructor(private layoutStoreFacade: LayoutStoreFacade,
+  constructor(
+    private boardsStoreFacade: BoardsStoreFacade,
+    private layoutStoreFacade: LayoutStoreFacade,
               private modalService: ModalService) { }
 
   ngOnInit(): void {
