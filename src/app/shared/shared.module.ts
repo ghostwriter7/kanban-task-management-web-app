ece1d63dae9df0b2f9940855591eca9886482035
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-06-17 20:58:39 +0200

    Context Menu created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {AddButtonComponent} from './add-button/add-button.component';
 import {SpinnerComponent} from './spinner/spinner.component';
+import { ContextMenuComponent } from './context-menu/context-menu.component';
 
 @NgModule({
-    declarations: [SpinnerComponent, AddButtonComponent],
+    declarations: [SpinnerComponent, AddButtonComponent, ContextMenuComponent],
     imports: [
         CommonModule
     ],
-    exports: [SpinnerComponent, AddButtonComponent]
+  exports: [SpinnerComponent, AddButtonComponent, ContextMenuComponent]
 })
 export class SharedModule {}
